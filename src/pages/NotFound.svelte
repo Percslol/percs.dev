f23e0af63b2b3c8746641d8278828da75d1cd53d
Percs <83934299+Percslol@users.noreply.github.com>
2024-08-19 19:36:13 -0500

    add go home to 404 page

--- src/pages/NotFound.svelte
+++ src/pages/NotFound.svelte
@@ -1,1 +1,9 @@
-<div class="flex justify-center items-center font-sans">404</div>
+<script lang="ts">
+    import Button from "../components/Button.svelte";
+</script>
+<div class="fixed flex flex-col items-center justify-center w-screen h-screen">
+    <div class="flex justify-center items-center font-sans">404</div>
+    <div class="flex flex-row">
+        <Button href="/#/">Go Home</Button>
+    </div>
+</div>
