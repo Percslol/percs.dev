83586e4b56bc0d46ba352149239316becfad0e7c
percs <me@percs.dev>
2025-11-10 01:25:22 -0600

    more format

--- src/main-client.ts
+++ src/main-client.ts
@@ -11,11 +11,11 @@ hydrate(
 function atkinson(pixels: number[], w: number): boolean[] {
 	const e = Array(2 * w).fill(0);
 	const m = [0, 1, w - 2, w - 1, w, 2 * w - 1];
-	return pixels.map(x => {
+	return pixels.map((x) => {
 		const pix = x + (e.push(0), e.shift()!);
 		const col = pix > 1.5;
 		const err = (pix - (col ? 1 : 0)) / 8;
-		m.forEach(x => e[x] += err);
+		m.forEach((x) => (e[x] += err));
 		return col;
 	});
 }
@@ -26,24 +26,24 @@ const image = new Image();
 image.crossOrigin = "Anonymous";
 image.src = `https://picsum.photos/${width}/${height}`;
 image.onload = () => {
-	const canvas = document.createElement('canvas');
-	const ctx = canvas.getContext('2d')!;
+	const canvas = document.createElement("canvas");
+	const ctx = canvas.getContext("2d")!;
 	canvas.width = width;
 	canvas.height = height;
-	
+
 	ctx.drawImage(image, 0, 0, width, height);
 	const imageData = ctx.getImageData(0, 0, width, height);
 	const data = imageData.data;
-	
+
 	// Convert to grayscale
 	const grayscale = [];
 	for (let i = 0; i < data.length; i += 4) {
 		grayscale.push((data[i] + data[i + 1] + data[i + 2]) / 765);
 	}
-	
+
 	// Apply dithering
 	const dithered = atkinson(grayscale, width);
-	
+
 	// Apply colors
 	for (let i = 0; i < dithered.length; i++) {
 		const [r, g, b] = dithered[i] ? [128, 128, 128] : [0, 0, 0];
@@ -51,7 +51,7 @@ image.onload = () => {
 		data[i * 4 + 1] = g;
 		data[i * 4 + 2] = b;
 	}
-	
+
 	ctx.putImageData(imageData, 0, 0);
 	document.documentElement.style.background = `url(${canvas.toDataURL()})`;
 	document.documentElement.style.backgroundSize = "cover";
--- src/pages/ContactPage.tsx
+++ src/pages/ContactPage.tsx
@@ -3,8 +3,11 @@ import { type Component } from "dreamland/core";
 const ContactPage: Component = function () {
 	return (
 		<div>
-			<p>contact me:</p>
-			<span>you can contact me at any of the following, though i might be slow to respond on some.</span>
+			<h2>contact:</h2>
+			<span>
+				you can contact me at any of the following, though i might be slow to
+				respond on some.
+			</span>
 			<ul>
 				<li>
 					<a href="mailto:me@percs.dev">email</a>
@@ -15,7 +18,11 @@ const ContactPage: Component = function () {
 				<li>
 					<a href="https://discord.com/users/76755933984168346">discord</a>
 				</li>
-				<li><a href="https://signal.me/#eu/vxUhAa-YsPDRNc0BKGMeS_NklIm9AYnBvhGwodheym6ATgr3y6RG76XFJSa2FHrx">signal</a></li>
+				<li>
+					<a href="https://signal.me/#eu/vxUhAa-YsPDRNc0BKGMeS_NklIm9AYnBvhGwodheym6ATgr3y6RG76XFJSa2FHrx">
+						signal
+					</a>
+				</li>
 				<li>
 					<a href="https://x.com/percsisok">x</a>
 				</li>
