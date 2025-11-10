a7fcc66d7a4e28e3fca189cf15aac711aceea3dc
percs <me@percs.dev>
2025-11-10 01:42:14 -0600

    add more buttons + simple analytics

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -40,6 +40,15 @@ const Homepage: Component = function () {
 				<a href="https://velzie.rip">
 					<img src="https://velzie.rip/88x31.png" alt="velzie" />
 				</a>
+                <a href="https://wearr.dev">
+                    <img src="https://wearr.dev/88x31.gif" alt="wearr" />
+                </a>
+                <a href="https://foxmoss.com">
+                    <img src="https://foxmoss.com/foxmossbutton.png" alt="foxmoss" />
+                </a>
+				<a href="https://errorcodezero.dev/">
+					<img src="https://errorcodezero.dev/button.png" alt="errorcodezero's button" />
+				</a>
 			</section>
 		</div>
 	);
